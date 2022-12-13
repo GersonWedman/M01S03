323a47a32fef178325cd57931f8f6f7b34b3c895
GersonWedman <gerson_wedman-filho@estudante.sesisenai.org.br>
2022-12-13 16:51:26 -0300

    Exercício 1 - Trello

---
+++ Exercício01/README.md
@@ -0,0 +1,3 @@
+# M01S03 - Exercício Trello 01 
+#Exercício com objetivo de localizar o menor e o maior valor em um array numérico.
+#São informados os array que devem ser testados, com valores undefined, null, e iguais. 
---
+++ Exercício01/app.js
@@ -0,0 +1,20 @@
+var lista=[56, 65, 64, -3, 23, 42, -15, 65, 14, 32, 78, 70, 47];
+
+function procuraMinMax(lista){
+lista.sort;
+a=lista[0];
+b=lista[lista.lenght-1];
+
+if(a<b){
+  alert('menor é : ' +a);
+  alert('maior é : ' +b);
+}
+else {
+  alert('Não é possível encontrar');
+}
+}
+var lista=[56, 65, 64, -3, 23, 42, -15, 65, 14, 32, 78, 70, 47];
+procuraMinMax(lista);
+procuraMinMax(56, 65, 64, -3, 23, 42, -15, 65, 14, 32, 78, 70, 47);
+
+
