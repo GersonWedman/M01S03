a8e6cec7757fa21bcc7ccd391db6ed04e95add1c
GersonWedman <gerson_wedman-filho@estudante.sesisenai.org.br>
2022-12-13 20:14:13 -0300

    Aula 02

---
+++ aula02/app.js
@@ -0,0 +1,47 @@
+// //script aula 02
+// //exemplo de inserção e remoção ao final do array
+/*
+// const vetorExemplo = [];
+
+// vetorExemplo.push(1);
+// vetorExemplo.push('a');
+// vetorExemplo.push('ultimo');
+
+// //pegar o último elemento, atribuir a uma variável e remover da pilha
+// const ultimo = vetorExemplo.pop()
+
+// console.log(vetorExemplo);
+// //--------------------------------
+*/
+
+//FUNÇÕES 
+
+// function f(x, y){
+// return x**2 + y;
+// }
+
+// //exemplo verificação parametros
+// function funcaoExemplo(x,y){
+//   if(typeof x !== 'number' || typeof y !== 'number'){
+//     return;
+//   }
+// }
+
+// const resultado = f(2, 3);
+// const resultadoB = f(5, 8);
+
+// console.log(resultado)
+// console.log(resultadoB)
+
+// // -----------------------------
+
+function ola(nome, idade){
+console.log(`Olá, ${nome}!!! Você tem ${idade} anos!!!`);
+}
+
+ola('Gerson', 32)
+ola('João', 30)
+
+
+
+
