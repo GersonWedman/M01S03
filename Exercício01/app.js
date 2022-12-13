83d2724eb49f6268628bc89388ea6bbbdde3c339
GersonWedman <gerson_wedman-filho@estudante.sesisenai.org.br>
2022-12-13 17:30:14 -0300

    Exercício 01 - Trello Validado o funcionamento.

--- Exercício01/app.js
+++ Exercício01/app.js
@@ -1,20 +1,45 @@
 var lista=[56, 65, 64, -3, 23, 42, -15, 65, 14, 32, 78, 70, 47];
+var tamanho = 0;
 
-function procuraMinMax(lista){
-lista.sort;
-a=lista[0];
-b=lista[lista.lenght-1];
 
-if(a<b){
-  alert('menor é : ' +a);
-  alert('maior é : ' +b);
-}
-else {
-  alert('Não é possível encontrar');
-}
-}
-var lista=[56, 65, 64, -3, 23, 42, -15, 65, 14, 32, 78, 70, 47];
-procuraMinMax(lista);
-procuraMinMax(56, 65, 64, -3, 23, 42, -15, 65, 14, 32, 78, 70, 47);
+function funcaoComumA(valor){
+  console.log('Desordenado ' + valor);
+  //ordenação da lista e impressão
+  valor.sort();
+  console.log(typeof(valor));
+  console.log(valor);
+  //det do menor
+  var menor = valor[0]
+  //det do maior
+  var maior=valor[lista.length-1];
+  let tamanho=lista.length-1;
+  if(menor<maior){
+    console.log('Menor é = ' + menor)
+    console.log('Maior é = ' + maior)
+  }
+
+  else {
+    console.log('Não é possível determinar o maior e menor')
+  }
+
+  }
+  funcaoComumA(lista);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
