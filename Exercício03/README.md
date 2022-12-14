ff601cc172a17bc716a0a8ed7d72759300ca65c4
GersonWedman <gerson_wedman-filho@estudante.sesisenai.org.br>
2022-12-14 16:06:24 -0300

    Exercício 03 - Trello

---
+++ Exercício03/README.md
@@ -0,0 +1,4 @@
+# M01S03 - Exercício Trello 03
+
+#Exercício com objetivo de concatenar 2 arrays através de operador Spread(...)
+#Resultado é um único array com todos valores das 2 array.
---
+++ Exercício03/app.js
@@ -0,0 +1,8 @@
+// Em app.js desenvolva uma função que receba dois arrays e realize a concatenação entre eles utilizando o operador Spread.
+// Apresente o resultado no console.log(novoArray).
+const arrA = [1, 2, 3];
+const arrB = [4, 5, 6];
+
+const arrC = [...arrA, ...arrB];
+
+console.log(arrC);
