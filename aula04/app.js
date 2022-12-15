93288010e109b9126bdaaa498f4968c97932199d
GersonWedman <gerson_wedman-filho@estudante.sesisenai.org.br>
2022-12-15 19:14:56 -0300

    Aula 04

--- aula04/app.js
+++ aula04/app.js
@@ -1,10 +1,2 @@
 //REST
-//EXERCÍCIO 7 - TRELLO
-
-const somaTodos = (inicial, ...numeros) => {
-  return numeros.reduce((acum, n) => {
-    return acum + n;
-  }, inicial);
-};
-
-console.log(somaTodos(2, 4, 5, 7, 8));
+//AULA 4
